87faba1a42920b1f62ecfbddb26188b8e5a6cff0
Jill Ashey <jillashey@Jills-MacBook-Pro.local>
2020-11-16 18:44:28 -0500

    change folder name

---
+++ basic_stats/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+basic_stats Package for Python
+    Version 1.0 Nov. 16, 2020
+    Author:  Jill Ashey, URI.
+    Email: jillashey@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .basic_stats import *
+
+
+__all__ = ['sat_vp']
